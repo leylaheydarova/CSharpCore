6966eac83ad6c4e398a5c0d64a21834185b94887
leylaheydarova <leylaheydarova.dev@gmail.com>
2025-12-20 12:25:25 +0400

    TryParse, ForEach, Do-While

---
+++ CSharpCore/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = 50;
+while (number > 10)
+{
+    Console.WriteLine("Hello!");
+    number /= 2;
+    if (number < 22) continue; //oldugu setirden asagi getmesine izn vermir,
+                               //scope-un evveline qaytarir
+    Console.WriteLine($"I am {number}");
+}
